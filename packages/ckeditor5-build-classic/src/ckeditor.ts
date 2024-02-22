27c739ec2503bbebd0cc7257a4ce36384b276527
Zaki syed <syedzaki.uddin@yahoo.com>
2024-02-23 02:47:14 +0530

    Update ckeditor.ts

--- packages/ckeditor5-build-classic/src/ckeditor.ts
+++ packages/ckeditor5-build-classic/src/ckeditor.ts
@@ -1,124 +1,79 @@
-/**
- * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
-
-// The editor creator to use.
-import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-classic';
-import { GeneralHtmlSupport } from '@ckeditor/ckeditor5-html-support';
-
-import { Essentials } from '@ckeditor/ckeditor5-essentials';
-import { CKFinderUploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder';
-import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
-import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
-import { CKBox } from '@ckeditor/ckeditor5-ckbox';
-import { FindAndReplace } from '@ckeditor/ckeditor5-find-and-replace';
-import { CKFinder } from '@ckeditor/ckeditor5-ckfinder';
-import { EasyImage } from '@ckeditor/ckeditor5-easy-image';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload, PictureEditing } from '@ckeditor/ckeditor5-image';
-import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
-import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
-import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
-import { TextTransformation } from '@ckeditor/ckeditor5-typing';
-import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
-import { Plugin } from '@ckeditor/ckeditor5-core';
-
-class TranscriptHighlighter extends Plugin {
-	init() {
-		const editor = this.editor;
-
-		// Allow bold attribute on text nodes.
-		editor.model.schema.extend( '$text', { allowAttributes: 'highlight' } );
-		editor.model.schema.setAttributeProperties( 'highlight', {
-			isFormatting: true,
-			copyOnEnter: true
-		} );
-
-        // Build converter from model to view for data and editing pipelines.
-		editor.conversion.attributeToElement( {
-			model: 'highlight',
-			view: 'mark'
-        });
-
-	}
-}
-
-export default class ClassicEditor extends ClassicEditorBase {
-	public static override builtinPlugins = [
-		Essentials,
-		CKFinderUploadAdapter,
-		Autoformat,
-		Bold,
-		Italic,
-		BlockQuote,
-		CKBox,
-		CKFinder,
-		CloudServices,
-		EasyImage,
-		TranscriptHighlighter,
-		FindAndReplace,
-		Heading,
-		Image,
-		ImageCaption,
-		ImageStyle,
-		ImageToolbar,
-		ImageUpload,
-		Indent,
-		Link,
-		GeneralHtmlSupport,
-		List,
-		MediaEmbed,
-		Paragraph,
-		PasteFromOffice,
-		PictureEditing,
-		Table,
-		TableToolbar,
-		TextTransformation
-	];
-
-	public static override defaultConfig = {
-		toolbar: {
-			items: [
-				'fullpage',
-				'findAndReplace',
-				'undo',
-				'redo',
-				'bold', 'italic', 'underline', 'strikethrough', 'code', 'subscript', 'superscript'
-			]
-		},	
-		htmlSupport: {
-			allow: [  {
-				name: /.*/,
-				attributes: true,
-				classes: true,
-				styles: true
-				},
-			]
-		},
-		image: {
-			toolbar: [
-				'imageStyle:inline',
-				'imageStyle:block',
-				'imageStyle:side',
-				'|',
-				'toggleImageCaption',
-				'imageTextAlternative'
-			]
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
-			]
-		},
-		// This value must be kept in sync with the language defined in webpack.config.js.
-		language: 'en'
-	};
+{
+  "name": "@ckeditor/ckeditor5-build-classic",
+  "version": "41.1.0",
+  "description": "The classic editor build of CKEditor 5 - the best browser-based rich text editor.",
+  "keywords": [
+    "ckeditor5-build",
+    "ckeditor",
+    "ckeditor5",
+    "ckeditor 5",
+    "wysiwyg",
+    "rich text",
+    "editor",
+    "html",
+    "contentEditable",
+    "editing",
+    "operational transformation",
+    "ot",
+    "collaboration",
+    "collaborative",
+    "real-time",
+    "framework"
+  ],
+  "main": "./build/ckeditor.js",
+  "types": "./build/ckeditor.d.ts",
+  "files": [
+    "build",
+    "ckeditor5-metadata.json",
+    "CHANGELOG.md"
+  ],
+  "dependencies": {
+    "@ckeditor/ckeditor5-adapter-ckfinder": "41.1.0",
+    "@ckeditor/ckeditor5-autoformat": "41.1.0",
+    "@ckeditor/ckeditor5-basic-styles": "41.1.0",
+    "@ckeditor/ckeditor5-block-quote": "41.1.0",
+    "@ckeditor/ckeditor5-ckbox": "41.1.0",
+    "@ckeditor/ckeditor5-ckfinder": "41.1.0",
+    "@ckeditor/ckeditor5-cloud-services": "41.1.0",
+    "@ckeditor/ckeditor5-easy-image": "41.1.0",
+    "@ckeditor/ckeditor5-editor-classic": "41.1.0",
+    "@ckeditor/ckeditor5-essentials": "41.1.0",
+    "@ckeditor/ckeditor5-heading": "41.1.0",
+    "@ckeditor/ckeditor5-image": "41.1.0",
+    "@ckeditor/ckeditor5-indent": "41.1.0",
+    "@ckeditor/ckeditor5-link": "41.1.0",
+    "@ckeditor/ckeditor5-list": "41.1.0",
+    "@ckeditor/ckeditor5-find-and-replace": "^41.1.0",
+    "@ckeditor/ckeditor5-html-embed": "^41.1.0",
+    "@ckeditor/ckeditor5-html-support": "^41.1.0",
+    "@ckeditor/ckeditor5-inspector": "^4.1.0",
+    "@ckeditor/ckeditor5-media-embed": "41.1.0",
+    "@ckeditor/ckeditor5-paragraph": "41.1.0",
+    "@ckeditor/ckeditor5-paste-from-office": "41.1.0",
+    "@ckeditor/ckeditor5-table": "41.1.0",
+    "@ckeditor/ckeditor5-typing": "41.1.0"
+  },
+  "devDependencies": {
+    "@ckeditor/ckeditor5-core": "41.1.0",
+    "@ckeditor/ckeditor5-dev-translations": "^39.0.0",
+    "@ckeditor/ckeditor5-dev-utils": "^39.0.0",
+    "@ckeditor/ckeditor5-theme-lark": "41.1.0",
+    "terser-webpack-plugin": "^4.2.3",
+    "typescript": "5.0.4",
+    "webpack": "^5.58.1",
+    "webpack-cli": "^4.9.0"
+  },
+  "author": "CKSource (http://cksource.com/)",
+  "license": "GPL-2.0-or-later",
+  "homepage": "https://ckeditor.com/ckeditor-5",
+  "bugs": "https://github.com/ckeditor/ckeditor5/issues",
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/ckeditor/ckeditor5.git",
+    "directory": "packages/ckeditor5-build-classic"
+  },
+  "scripts": {
+    "build": "webpack --mode production",
+    "postbuild": "tsc -p ./tsconfig.types.json"
+  }
 }
